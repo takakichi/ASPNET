544f21e2b048a38d97a539a4ad6e43d16894ce86
takakichi <antlers.takapon.kun@gmail.com>
2019-11-10 23:49:23 +0900

    sample02ソリューションの追加

---
+++ Prototype/sample02/sample02/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="sample02.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="stylesheet" href="./css/boostrap/bootstrap.css" />
+    <link rel="stylesheet" href="./css/base.css" />
+    <script type="text/javascript" src="./Scripts/jquery/jquery-1.12.4.js"></script>
+    <script type="text/javascript" src="./Scripts/bootstrap/bootstrap.bundle.js"></script>
+    <script type="text/javascript" src="./Scripts/base.js"></script>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <!-- ナビゲーションバー -->
+        <nav class="navbar navbar-dark bg-dark">
+            <a href="#" class="navbar-brand">
+                <img src="./image/popase.gif" width="30" height="30" alt=""/><asp:Label runat="server" ID="lblSystemName">○○○○○</asp:Label>
+            </a>
+        </nav>
+        <div  class="container">
+            <!-- メインコンテンツ -->
+            <main>
+                <div class="row">
+                このプロジェクトは、Bootstrap4 + ASP.NET(C#) を使って色々試しているサンプルプロジェクトです。<br />
+                本プロジェクトのボタンイメージは、 <a href="http://box.aflat.com/buttonmaker/">Button Makerさん</a>を使って作っています。
+                </div>
+                <div class="row">
+                    <asp:ImageButton runat="server" ID="imgBtn01" ImageUrl="~/Images/btnMenu01.gif" /><br />
+                    <asp:ImageButton runat="server" ID="imgBtn02" ImageUrl="~/Images/btnMenu02.gif" /><br />
+                    <asp:ImageButton runat="server" ID="imgBtn03" ImageUrl="~/Images/btnMenu03.gif" /><br />
+                    <asp:ImageButton runat="server" ID="imgBtn04" ImageUrl="~/Images/btnMenu04.gif" /><br />
+                    <asp:ImageButton runat="server" ID="imgBtn05" ImageUrl="~/Images/btnMenu05.gif" /><br />
+                </div>
+            </main>
+            <!-- フッダコンテンツ -->
+            <footer>
+            </footer>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Prototype/sample02/sample02/Default.aspx.designer.cs
@@ -0,0 +1,78 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace sample02 {
+    
+    
+    public partial class Default {
+        
+        /// <summary>
+        /// form1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// lblSystemName コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSystemName;
+        
+        /// <summary>
+        /// imgBtn01 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton imgBtn01;
+        
+        /// <summary>
+        /// imgBtn02 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton imgBtn02;
+        
+        /// <summary>
+        /// imgBtn03 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton imgBtn03;
+        
+        /// <summary>
+        /// imgBtn04 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton imgBtn04;
+        
+        /// <summary>
+        /// imgBtn05 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton imgBtn05;
+    }
+}
